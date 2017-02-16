585c142b8821bc2c6d829dab86da2348ea4bc2a8
ayan24365 <ayan.nigam+github@gmail.com>
2017-02-16 18:09:37 +0530

    Add existing file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
+## Put comments here that give an overall des
+## Write a short commencription of what your
 ## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+MakeCacheMatrix<-function(AyanM=matrix())
+{
+Inv<-NULL
+set<-function(y) {
+AyanM<<-y
+inv<<-NULL
 }
+get<-function()AyanM
+setinverse <- function(inverse) inv <<- inverse
+getinverse <- function() inv
+list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		
+		makeCacheMatrix()
+        
+        inv = x$getinv()
+        
+                if (!is.null(inv)){
+                message("Obtaining cached data")
+                return(inv)
+        }
+        
+        
+        mat.data = x$get()
+        inv = solve(mat.data, ...)
+        
+        x$setinv(inv)
+        
+        return(inv)
+
 }
+
+
+
